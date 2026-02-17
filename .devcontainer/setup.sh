992f90e6a28ad5a13c2271c3d765a6209a172efc
SasankaPandaSCIT <105026417+SasankaPandaSCIT@users.noreply.github.com>
2026-02-17 11:36:09 -0600

    Update model version in setup script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -39,7 +39,8 @@ schema: v1
 models:
   - name: OpenAI-via-Helicone-Proxy
     provider: openai
-    model: gpt-4o
+    #model: gpt-4o
+    model: gpt-5.2 
     apiBase: https://ai-gateway.helicone.ai/v1
     apiKey: '$API_KEY'
 roles:
